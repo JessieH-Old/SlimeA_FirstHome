d48d6537bce761a7b92ef83be150c5a5efcb8c68
Jessie.H <a_800907@hotmail.com>
2021-06-28 02:03:07 +0800

    complete

---
+++ js/index.js
@@ -0,0 +1,69 @@
+function imgrolling() {
+    if (document.getElementById('img1').src === document.getElementById('img6').src) {
+        document.getElementById('start').classList.add('d-none');
+        document.getElementById('ready').classList.remove('d-none');
+    } else {
+        document.getElementById('start').innerHTML = 'Next >>>>>>';
+        document.getElementById('img1').src = document.getElementById('img2').src;
+        document.getElementById('img2').src = document.getElementById('img3').src;
+        document.getElementById('img3').src = document.getElementById('img4').src;
+        document.getElementById('img4').src = document.getElementById('img5').src;
+        document.getElementById('img5').src = document.getElementById('img6').src;
+    }
+}
+
+function imReady() {
+    window.alert('史老師~~~~~');
+    window.alert('恭祝你福壽與天齊');
+    window.alert('慶賀你生辰快樂');
+    window.alert('年年都有今日');
+    window.alert('歲歲都有今朝');
+    window.alert('恭喜你！');
+    window.alert('恭喜你！');
+    window.alert('欸等等');
+    window.alert('還沒結束');
+    window.alert('我要在再唱一首');
+    window.alert('祝你生日快樂～');
+    window.alert('祝你生日快樂～');
+    window.alert('祝你生日快樂兒～');
+    window.alert('祝你生日快樂！');
+    window.alert('英文版一起唱～');
+    window.alert('Happy Birthday to you~');
+    window.alert('Happy Birthday to you~');
+    window.alert('Happy Birthday to you woo~');
+    window.alert('Happy Birthday to you~');
+    window.alert('不要急著關掉我');
+    window.alert('我還沒完');
+    window.alert('啦');
+    window.alert('啦');
+    window.alert('啦');
+    window.alert('啦');
+    window.alert('啦');
+    window.alert('啦');
+    window.alert('啦');
+    window.alert('啦');
+    window.alert('啦');
+    window.alert('啦');
+    window.alert('啊');
+    window.alert('啊');
+    window.alert('啊');
+    window.alert('啊');
+    window.alert('啊');
+    window.alert('啊');
+    window.alert('啊');
+    window.alert('啊');
+    window.alert('呀');
+    window.alert('呀');
+    window.alert('呀');
+    window.alert('呀');
+    window.alert('呀');
+    window.alert('呀');
+    window.alert('呀');
+    window.alert('呀');
+    window.alert('呀');
+    window.alert('呀');
+    window.alert('呀');
+    window.alert('呀');
+    window.alert('呀');
+    window.alert('好啦祝史老師生日快樂～～～(如果你有按到最後才會收到我最真摯的祝福XD)');
+}
